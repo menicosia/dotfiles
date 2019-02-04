b59500fc1d4b3c43142319262c77addff8ff2372
Marco Nicosia <mnicosia@pivotal.io>
2019-02-04 14:56:12 -0800

    - Convert bosh2 to just bosh - Add pws-vpn alias to help me get to production bosh

--- pivotal/bash_it.aliases.bash
+++ pivotal/bash_it.aliases.bash
@@ -5,10 +5,10 @@ alias pxc="cd ~/workspace/pxc-release"
 
 alias gpc="git pair-commit"
 
-alias gosri="bosh2 -e sriracha"
+alias gosri="bosh -e sriracha"
 alias sshsri="gosri -d cf-mysql ssh --gw-user jumpbox --gw-host 35.193.197.242 --gw-private-key ~/.ssh/sriracha.id_rsa"
-alias targetsri='(CF_CONFIG=$(find $HOME/workspace/core-services-oss-env-resource-pool -name sriracha); CF_VARS=$HOME/workspace/deployments-core-services/sriracha/cf-deployment-vars.yml; cf api $(jq -r "\"api.\" + .domain" $CF_CONFIG) --skip-ssl-validation; cf login -u admin -p $(bosh2 int --path /cf_admin_password $CF_VARS))'
-alias srilogin='(cd $HOME/workspace/deployments-core-services/sriracha; git pull; bbl4 ssh-key > ~/.ssh/sriracha.id_rsa; chmod 600 ~/.ssh/sriracha.id_rsa; BOSH_CA_CERT=$(bbl4 director-ca-cert) bosh2 login --client=$(bbl4 director-username) --client-secret=$(bbl4 director-password) -e $(bbl4 director-address); BOSH_CA_CERT=$(bbl4 director-ca-cert) bosh2 -e $(bbl4 director-address) alias-env sriracha)'
+alias targetsri='(CF_CONFIG=$(find $HOME/workspace/core-services-oss-env-resource-pool -name sriracha); CF_VARS=$HOME/workspace/deployments-core-services/sriracha/cf-deployment-vars.yml; cf api $(jq -r "\"api.\" + .domain" $CF_CONFIG) --skip-ssl-validation; cf login -u admin -p $(bosh int --path /cf_admin_password $CF_VARS))'
+alias srilogin='(cd $HOME/workspace/deployments-core-services/sriracha; git pull; bbl4 ssh-key > ~/.ssh/sriracha.id_rsa; chmod 600 ~/.ssh/sriracha.id_rsa; BOSH_CA_CERT=$(bbl4 director-ca-cert) bosh login --client=$(bbl4 director-username) --client-secret=$(bbl4 director-password) -e $(bbl4 director-address); BOSH_CA_CERT=$(bbl4 director-ca-cert) bosh -e $(bbl4 director-address) alias-env sriracha)'
 alias dammit="bosh -n cleanup && bosh create release --force && bosh -n upload release"
 alias sridammit="gosri -n clean-up && gosri create-release --force && gosri -n upload-release"
 function cfadminc ()
@@ -27,4 +27,10 @@ function funneltunnel () {
     pushd ${HOME}/workspace/funnel-api.node/
     cf ssh -N -L 63306:q-n8s3y1.q-g190.bosh:3306 funnel-api &
     popd
+    cf service-key funnelDB marco-desktop | tail +2 | jq -r .password | pbcopy
+}
+function pws-vpn () {
+    echo 'Instructions at: https://github.com/pivotal-cloudops/cloudops-docs/blob/master/bosh-director-access-openvpn-ssoca.md'
+    lpass show --clip --password 'Shared-PWS-mnicosia-Dropbox/PWS BOSH mnicosia'
+    bosh -e prod login
 }
