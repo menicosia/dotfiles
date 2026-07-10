bd63a940374b97affff2396dadb819a79a17c88a
Marco Nicosia <marco-github@escape.org>
2026-07-09 22:21:50 -0700

    complete move of bash_profile to bashrc, move a lot of PATH stuff to local-setings.

--- marco/local-settings.bash
+++ marco/local-settings.bash
@@ -1,7 +1,19 @@
 #
 echo "sourcing local settings..."
 
-if [ -d "/opt/homebrew" ]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-  [ -d "/opt/homebrew/opt/mysql-client" ] && PATH+=":/opt/homebrew/opt/mysql-client/bin"
-fi
+[ -d "/opt/homebrew" ] && eval `/opt/homebrew/bin/brew shellenv ${SHELL##*/}`
+[ -d "/opt/homebrew/opt/mysql-client" ] && PATH+=":/opt/homebrew/opt/mysql-client/bin"
+[ -d "/opt/homebrew/opt/python/libexec/bin" ] && PATH+=":/opt/homebrew/opt/python/libexec/bin"
+
+[[ ":$PATH:" != *":$HOME/.local/bin"* ]] && PATH+=":$HOME/.local/bin"
+[ -d /usr/local/sbin ] && PATH+=":/usr/local/sbin"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '$HOME/google-cloud-sdk/path.bash.inc' ]; then source '$HOME/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+# if [ -f '$HOME/google-cloud-sdk/completion.bash.inc' ]; then source '$HOME/google-cloud-sdk/completion.bash.inc'; fi
+
+[ -d /usr/local/opt/chruby ] && source /usr/local/opt/chruby/share/chruby/chruby.sh
+
+export PATH ;
